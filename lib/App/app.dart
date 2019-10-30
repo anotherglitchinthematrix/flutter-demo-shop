cfa4d60597489a9d479b7b10383e804ff95f27db
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-30 16:52:57 +0300

    Initial App structure

---
+++ lib/App/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:course_008/App/pages/index.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,5 @@
+import 'package:flutter/material.dart';
 
+import 'package:course_008/App/app.dart';
+
+void main() => runApp(App());
