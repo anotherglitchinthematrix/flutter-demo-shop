0e7d97c726e7c3e658773283733c3c53136b3f62
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-31 17:21:03 +0300

    Remove ugly favorites filter

--- lib/App/pages/products/products.page.dart
+++ lib/App/pages/products/products.page.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:course_008/App/widgets/index.dart';
 
-class ProductsPage extends StatefulWidget {
+class ProductsPage extends StatelessWidget {
   static const routeName = 'products';
-
-  @override
-  _ProductsPageState createState() => _ProductsPageState();
-}
-
-class _ProductsPageState extends State<ProductsPage> {
-  bool _showFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Shop App'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(_showFavorites ? Icons.radio_button_checked : Icons.radio_button_unchecked),
-            onPressed: () {
-              setState(() {
-                _showFavorites = !_showFavorites;
-              });
-            },
-          ),
-        ],
       ),
-      body: ProductsGrid(
-        showFavorites: _showFavorites,
-      ),
+      body: ProductsGrid(),
     );
   }
 }
