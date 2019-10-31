a49c595e77b6501f82363454b0ac739670ae2401
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-31 10:02:07 +0300

    ProductItem tweak

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -5,6 +5,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
       home: ProductsPage(),
     );
   }
--- lib/App/widgets/productItem/productItem.widget.dart
+++ lib/App/widgets/productItem/productItem.widget.dart
@@ -8,10 +8,57 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        product.imageURL,
-        fit: BoxFit.cover,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          color: Theme.of(context).primaryColorDark,
+          width: 1,
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: GridTile(
+          child: Image.network(
+            product.imageURL,
+            fit: BoxFit.cover,
+          ),
+          header: Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              color: Colors.pink,
+              icon: Icon(Icons.favorite_border),
+              onPressed: () {},
+            ),
+          ),
+          footer: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: Theme.of(context).primaryColor.withAlpha(128),
+                  width: 1,
+                ),
+              ),
+            ),
+            child: GridTileBar(
+              backgroundColor: Theme.of(context).primaryColorDark.withAlpha(48),
+              title: Text(
+                product.title,
+                overflow: TextOverflow.fade,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              trailing: IconButton(
+                color: Theme.of(context).primaryColor,
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
