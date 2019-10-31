7659103ae87b0ac22064f3c635804a3ba975208a
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-10-31 11:26:09 +0300

    Navigation to ProductPage

--- lib/App/widgets/productItem/productItem.widget.dart
+++ lib/App/widgets/productItem/productItem.widget.dart
@@ -1,3 +1,4 @@
+import 'package:course_008/App/pages/index.dart';
 import 'package:flutter/material.dart';
 import 'package:course_008/App/models/index.dart';
 
@@ -19,44 +20,54 @@ class ProductItem extends StatelessWidget {
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(8),
-        child: GridTile(
-          child: Image.network(
-            product.imageURL,
-            fit: BoxFit.cover,
-          ),
-          header: Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              color: Colors.pink,
-              icon: Icon(Icons.favorite_border),
-              onPressed: () {},
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) {
+                return ProductPage(product);
+              }),
+            );
+          },
+          child: GridTile(
+            child: Image.network(
+              product.imageURL,
+              fit: BoxFit.cover,
             ),
-          ),
-          footer: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  color: Theme.of(context).primaryColor.withAlpha(96),
-                  width: 1,
-                ),
-              ),
-            ),
-            child: GridTileBar(
-              backgroundColor: Theme.of(context).primaryColorDark.withAlpha(48),
-              title: Text(
-                product.title,
-                overflow: TextOverflow.fade,
-                style: TextStyle(
-                  color: Theme.of(context).primaryColorDark,
-                  fontFamily: 'Inter',
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              trailing: IconButton(
-                color: Theme.of(context).primaryColor,
-                icon: Icon(Icons.shopping_cart),
+            header: Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                color: Colors.pink,
+                icon: Icon(Icons.favorite_border),
                 onPressed: () {},
+              ),
+            ),
+            footer: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: Theme.of(context).primaryColor.withAlpha(96),
+                    width: 1,
+                  ),
+                ),
+              ),
+              child: GridTileBar(
+                backgroundColor:
+                    Theme.of(context).primaryColorDark.withAlpha(48),
+                title: Text(
+                  product.title,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColorDark,
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                trailing: IconButton(
+                  color: Theme.of(context).primaryColor,
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {},
+                ),
               ),
             ),
           ),
