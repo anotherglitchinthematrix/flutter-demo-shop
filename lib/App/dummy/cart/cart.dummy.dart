624189af5aa5edb49ce96c5815288bf7d4b5c4ad
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-11-01 12:08:29 +0300

    Dummy cart

---
+++ lib/App/dummy/cart/cart.dummy.dart
@@ -0,0 +1,20 @@
+import 'package:course_008/App/dummy/index.dart';
+import 'package:course_008/App/providers/index.dart';
+
+final List<CartItem> dummyCart = [
+  CartItem(
+    id: 'c1',
+    product: dummyProducts[0],
+    quantity: 2,
+  ),
+  CartItem(
+    id: 'c2',
+    product: dummyProducts[1],
+    quantity: 4,
+  ),
+  CartItem(
+    id: 'c3',
+    product: dummyProducts[2],
+    quantity: 6,
+  ),
+];
--- lib/App/dummy/index.dart
+++ lib/App/dummy/index.dart
@@ -1,1 +1,2 @@
 export 'products/products.dummy.dart';
+export 'cart/cart.dummy.dart';
