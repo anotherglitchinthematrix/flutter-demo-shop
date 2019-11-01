414ac25db077f1d5debc919dc8b3655201e60122
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-11-01 14:23:15 +0300

    Styling

--- lib/App/widgets/cart/cart.item.dart
+++ lib/App/widgets/cart/cart.item.dart
@@ -1,3 +1,4 @@
+import 'package:course_008/App/pages/index.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:course_008/App/providers/index.dart';
@@ -10,63 +11,70 @@ class CartListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var product = item.product;
-    return Card(
-      // color: Colors.blue,
-      margin: EdgeInsets.only(bottom: 8),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(4),
-        child: Dismissible(
-          key: ValueKey(item.id),
-          background: Container(
-            alignment: Alignment.centerRight,
-            color: Colors.red,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          direction: DismissDirection.endToStart,
-          onDismissed: (_) {
-            Provider.of<CartProvider>(context).removeFromCart(item);
-          },
-          child: ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
-            // dense: true,
-            leading: Image.network(
-              product.imageURL,
-              fit: BoxFit.cover,
-            ),
-            title: Text(
-              product.title,
-              style: TextStyle(
-                color: Theme.of(context).primaryColorDark,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            subtitle: Text(
-              product.description,
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                _CardColumn(
-                  title: 'Price',
-                  subtitle: '\$ ${(item.quantity * product.price).toStringAsFixed(2)}',
-                  alignment: CrossAxisAlignment.end,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(ProductPage.routeName, arguments: ProductPageArgument(product: item.product));
+      },
+      child: Card(
+        // color: Colors.blue,
+        margin: EdgeInsets.only(bottom: 8),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(4),
+          child: Dismissible(
+            key: ValueKey(item.id),
+            movementDuration: Duration(milliseconds: 200),
+            resizeDuration: Duration(milliseconds: 200),
+            background: Container(
+              alignment: Alignment.centerRight,
+              color: Colors.red,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
                 ),
-                _CardColumn(
-                  title: 'Qty',
-                  subtitle: '${item.quantity}',
-                  alignment: CrossAxisAlignment.center,
+              ),
+            ),
+            direction: DismissDirection.endToStart,
+            onDismissed: (_) {
+              Provider.of<CartProvider>(context).removeFromCart(item);
+            },
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+              // dense: true,
+              leading: Image.network(
+                product.imageURL,
+                fit: BoxFit.cover,
+              ),
+              title: Text(
+                product.title,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                  fontWeight: FontWeight.w500,
                 ),
-              ],
+              ),
+              subtitle: Text(
+                product.description,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  _CardColumn(
+                    title: 'Price',
+                    subtitle: '\$ ${(item.quantity * product.price).toStringAsFixed(2)}',
+                    alignment: CrossAxisAlignment.end,
+                  ),
+                  _CardColumn(
+                    title: 'Qty',
+                    subtitle: '${item.quantity}',
+                    alignment: CrossAxisAlignment.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
