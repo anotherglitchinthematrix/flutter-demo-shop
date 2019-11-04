fdecf42624b76bf8e28e1db57c44c5abd78b4f7f
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-11-04 11:52:58 +0300

    DummyOrder update

--- lib/App/dummy/order/order.dummy.dart
+++ lib/App/dummy/order/order.dummy.dart
@@ -4,13 +4,23 @@ import 'package:course_008/App/providers/index.dart';
 final List<OrderItem> dummyOrder = [
   OrderItem(
     id: 'o1',
-    total: 50,
+    total: 382.99,
     date: DateTime.now().add(Duration(days: -5)),
     cart: [
       CartItem(
         id: 'c1',
         product: dummyProducts[0],
         quantity: 3,
+      ),
+      CartItem(
+        id: 'c2',
+        product: dummyProducts[1],
+        quantity: 2,
+      ),
+      CartItem(
+        id: 'c3',
+        product: dummyProducts[2],
+        quantity: 1,
       ),
     ],
   ),
