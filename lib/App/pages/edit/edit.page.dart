a1622bd3f4d84f72d6b29e3652d4256e8e3c01fc
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-11-04 17:12:11 +0300

    BasicForm

--- lib/App/pages/edit/edit.page.dart
+++ lib/App/pages/edit/edit.page.dart
@@ -9,8 +9,53 @@ class EditPage extends StatefulWidget {
 }
 
 class _EditPageState extends State<EditPage> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                // maxLength: 128,
+                // maxLengthEnforced: false,
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                  hintText: 'Title of the product',
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_priceFocusNode),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Price',
+                  hintText: 'Price of the product',
+                ),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocusNode),
+                focusNode: _priceFocusNode,
+              ),
+              TextFormField(
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  hintText: 'Description of the product',
+                ),
+                focusNode: _descriptionFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
