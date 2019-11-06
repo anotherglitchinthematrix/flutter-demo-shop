aa29ef2a9f25863645303609cf47815adbfdcbd1
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2019-11-06 18:00:18 +0300

    Login form basic styling

--- lib/App/pages/authentication/authentication.page.dart
+++ lib/App/pages/authentication/authentication.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum AuthState { signIn, signUp }
+
 class AuthenticationPage extends StatelessWidget {
   static const routeName = 'auth';
 
@@ -12,33 +14,93 @@ class AuthenticationPage extends StatelessWidget {
         height: double.infinity,
         child: Center(
           child: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(32),
+              padding: EdgeInsets.all(8),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  AuthenticationForm(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AuthenticationForm extends StatefulWidget {
+  @override
+  _AuthenticationFormState createState() => _AuthenticationFormState();
+}
+
+class _AuthenticationFormState extends State<AuthenticationForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6),
+            boxShadow: [
+              BoxShadow(
+                color: Theme.of(context).primaryColorDark,
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Form(
             child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(32),
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(6),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Theme.of(context).primaryColorDark,
-                        blurRadius: 8,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
+                TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(8),
+                    labelText: 'Mail',
+                    border: InputBorder.none,
                   ),
-                  child: Container(),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                Divider(
+                  color: Theme.of(context).primaryColorLight,
+                  height: 0,
+                  thickness: 1,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(8),
+                    labelText: 'Password',
+                    border: InputBorder.none,
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: true,
+                ),
+                FlatButton(
+                  child: Text('Sign-in'),
+                  onPressed: () {},
                 ),
               ],
             ),
           ),
         ),
-      ),
+        FlatButton(
+          child: Text(
+            'Sign-up Instead',
+            style: TextStyle(
+              color: Theme.of(context).primaryColorLight,
+              fontSize: 12,
+            ),
+          ),
+          onPressed: () {},
+        ),
+      ],
     );
   }
 }
